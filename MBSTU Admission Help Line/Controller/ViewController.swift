92cff52ebcea064ed8de55347f7d371f3a8be733
Md Khaled Hasan Manna <khmannaict13@gmail.com>
2020-07-10 02:07:23 +0600

    hidden navigationBar for viewController

--- MBSTU Admission Help Line/Controller/ViewController.swift
+++ MBSTU Admission Help Line/Controller/ViewController.swift
@@ -26,9 +26,23 @@ class ViewController: UIViewController {
         
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
+    }
     
     //MARK:- Handlers
     @IBAction func enterBtnAction(_ sender: Any) {
+        
+        let homeVC = self.storyboard?.instantiateViewController(identifier: "HomeViewController") as! HomeViewController
+        self.navigationController?.pushViewController(homeVC, animated: true)
+       
+    
     }
     
 }
