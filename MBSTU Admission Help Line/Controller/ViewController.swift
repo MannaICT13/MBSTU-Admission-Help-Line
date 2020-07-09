31fa0931c1b9dcba3244520b6c75ce88b87d65ac
Md Khaled Hasan Manna <khmannaict13@gmail.com>
2020-07-09 18:39:11 +0600

    design UI and created outlets and action in ViewController

---
+++ MBSTU Admission Help Line/Controller/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  MBSTU Admission Help Line
+//
+//  Created by Md Khaled Hasan Manna on 9/7/20.
+//  Copyright © 2020 Md Khaled Hasan Manna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    //MARK:- Properties
+    @IBOutlet weak var mbstuLogoImg: UIImageView!
+    
+    @IBOutlet weak var enterBtnOutlet: UIButton!
+    
+    //MARK:- Initializzers
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        mbstuLogoImg.image = UIImage(named: "logo.png")
+        mbstuLogoImg.sizeToFit()
+    }
+
+    
+    //MARK:- Handlers
+    @IBAction func enterBtnAction(_ sender: Any) {
+    }
+    
+}
+
