8b3aa940216b46c802f6b53a1cef6d145fb5f650
Md Khaled Hasan Manna <khmannaict13@gmail.com>
2020-07-11 18:42:52 +0600

    created a tableView and TableViewCell with XIB

---
+++ MBSTU Admission Help Line/View/VivaView/VivaViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  VivaViewCell.swift
+//  MBSTU Admission Help Line
+//
+//  Created by Md Khaled Hasan Manna on 11/7/20.
+//  Copyright © 2020 Md Khaled Hasan Manna. All rights reserved.
+//
+
+import UIKit
+
+class VivaViewCell: UITableViewCell {
+
+    //MARK:- Properties
+    @IBOutlet weak var rollLblCell: UILabel!
+    @IBOutlet weak var positionLblCell: UILabel!
+    
+    @IBOutlet weak var subjectLblCell: UILabel!
+    @IBOutlet weak var statusLblCell: UILabel!
+    
+    
+    //MARK:- Initializers
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
