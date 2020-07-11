599e298dc7774256b0ebecae28c2c5cec65a753a
Md Khaled Hasan Manna <khmannaict13@gmail.com>
2020-07-11 15:05:04 +0600

    design UI and create outlets and action in ResultVC

---
+++ MBSTU Admission Help Line/Controller/ResultViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ResultViewController.swift
+//  MBSTU Admission Help Line
+//
+//  Created by Md Khaled Hasan Manna on 11/7/20.
+//  Copyright © 2020 Md Khaled Hasan Manna. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    //MARK:- Properties
+    
+    @IBOutlet weak var logoImg: UIImageView!
+    
+    @IBOutlet weak var unitTextField: UITextField!
+    @IBOutlet weak var rollTextField: UITextField!
+
+    @IBOutlet weak var searchBtnOutlet: UIButton!
+    
+    @IBOutlet weak var rollLbl: UILabel!
+    @IBOutlet weak var positionLbl: UILabel!
+    @IBOutlet weak var scoreLbl: UILabel!
+    @IBOutlet weak var statusLbl: UILabel!
+    
+    
+    
+    
+    
+    
+    
+    //MARK:- Initializers
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    
+    @IBAction func searchBtnAction(_ sender: Any) {
+        
+        
+        
+    }
+    
+    
+
+
+}
