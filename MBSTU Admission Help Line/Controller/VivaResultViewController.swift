8d036c6c8016dfcf1ee0e78e2560fd72096db13b
Md Khaled Hasan Manna <khmannaict13@gmail.com>
2020-07-11 22:18:12 +0600

    Implemented  tableView using SegmentedControl

--- MBSTU Admission Help Line/Controller/VivaResultViewController.swift
+++ MBSTU Admission Help Line/Controller/VivaResultViewController.swift
@@ -31,16 +31,21 @@ class VivaResultViewController: UIViewController {
         vivaResultB = Bundle.main.decode("v_b.json")
         vivaResultC = Bundle.main.decode("v_c.json")
         
-        
         self.tableView.register(UINib(nibName: "VivaViewCell", bundle: nil), forCellReuseIdentifier: "cell")
+        
+        manageSegmented()
+        
     }
     
     //MARK:- Handlers
-    
-    @IBAction func searchBtnAction(_ sender: Any) {
+    func manageSegmented(){
         
-       
+        segmentView.tintColor = UIColor.black
+        segmentView.backgroundColor = UIColor.gray
+        segmentView.selectedSegmentTintColor = UIColor.white
+        
     }
+    
     
     
 
@@ -53,16 +58,55 @@ extension VivaResultViewController : UITableViewDelegate,UITableViewDataSource{
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+       
+        if segmentView.selectedSegmentIndex == 0{
+            return vivaResultA.count
+        }else if segmentView.selectedSegmentIndex == 1{
+            return vivaResultB.count
+        }else if segmentView.selectedSegmentIndex == 2{
+            return vivaResultC.count
+        }
+        return 0
+        
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 100
+        return 120
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       
         let cell = self.tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! VivaViewCell
         
-        cell.rollLblCell.text = "111"
+        if segmentView.selectedSegmentIndex == 0{
+            
+          
+            
+            cell.rollLblCell.text = "ROLL:-\(vivaResultA[indexPath.row].roll ?? 0)"
+            cell.positionLblCell.text = "POSITION:-\(vivaResultA[indexPath.row].position ?? 0)"
+            cell.subjectLblCell.text = "SUBJECT:-\(vivaResultA[indexPath.row].subject ?? "n/a")"
+            cell.statusLblCell.text = "STATUS:-\(vivaResultA[indexPath.row].status ?? "n/a")"
+            
+            
+        }else if segmentView.selectedSegmentIndex == 1{
+            
+            cell.rollLblCell.text = "ROLL:-\(vivaResultB[indexPath.row].roll ?? 0) "
+            cell.positionLblCell.text = "POSITION:-\(vivaResultB[indexPath.row].position ?? 0)"
+            cell.subjectLblCell.text = "SUBJECT:-\(vivaResultB[indexPath.row].subject ?? "n/a")"
+            cell.statusLblCell.text = "STATUS:-\(vivaResultB[indexPath.row].status ?? "n/a")"
+          
+            
+        }else if segmentView.selectedSegmentIndex == 2{
+            
+            cell.rollLblCell.text = "ROLL:-\(vivaResultC[indexPath.row].roll ?? 0) "
+            cell.positionLblCell.text = "POSITION:-\(vivaResultC[indexPath.row].position ?? 0)"
+            cell.subjectLblCell.text = "SUBJECT:-\(vivaResultC[indexPath.row].subject ?? "n/a")"
+            cell.statusLblCell.text = "STATUS:-\(vivaResultC[indexPath.row].status ?? "n/a")"
+           
+            
+            
+        }
+        
+       
         return cell
     }
     
