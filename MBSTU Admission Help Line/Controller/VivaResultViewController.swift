3b903015967538cb35aa71c9fdfb9126e05f9e29
Md Khaled Hasan Manna <khmannaict13@gmail.com>
2020-07-11 18:54:40 +0600

    implement tableView delegate and Datasource method in VivaResultVC

--- MBSTU Admission Help Line/Controller/VivaResultViewController.swift
+++ MBSTU Admission Help Line/Controller/VivaResultViewController.swift
@@ -15,14 +15,18 @@ class VivaResultViewController: UIViewController {
     @IBOutlet weak var unitTextField: UITextField!
     @IBOutlet weak var searchBtnOutlet: UIButton!
     
+    @IBOutlet weak var tableView: UITableView!
     
     
     //MARK:-  Initializers
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        self.tableView.register(UINib(nibName: "VivaViewCell", bundle: nil), forCellReuseIdentifier: "cell")
 
-        // Do any additional setup after loading the view.
+     
         Utilities.borderTextField(textField: unitTextField)
         Utilities.roundBtnFill(button: searchBtnOutlet)
     }
@@ -35,4 +39,26 @@ class VivaResultViewController: UIViewController {
     
     
 
+}
+extension VivaResultViewController : UITableViewDelegate,UITableViewDataSource{
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! VivaViewCell
+        
+        cell.rollLblCell.text = "111"
+        return cell
+    }
+    
 }
